ed41633e7c73964bb5452d190acb1a3bc4cbc3d2
ryansldev <ryanlima1309@gmail.com>
2022-07-09 09:19:44 -0300

    feat: improving structure

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import "express-async-errors";
+import { errorHandler } from "~middlewares/errorHandler.middleware";
+import { router } from "~routes/router";
+
+const app = express();
+
+app.use(express.json());
+
+app.use(router);
+
+app.use(errorHandler);
+
+export { app };
---
+++ src/middlewares/errorHandler.middleware.ts
@@ -0,0 +1,41 @@
+import type { Request, Response, NextFunction } from "express";
+import { isCelebrateError } from "celebrate";
+import { AppException } from "~exceptions/App.exception";
+
+const errorHandler = (
+	err: Error,
+	_req: Request,
+	res: Response,
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
+	_next: NextFunction
+) => {
+	if (err instanceof AppException) {
+		return res.status(err.statusCode).json({ message: err.message });
+	}
+
+	if (isCelebrateError(err)) {
+		const types = ["body", "query", "headers"];
+
+		const errors = types.reduce((acc, type) => {
+			const error = err.details.get(type)?.details[0].message;
+
+			if (!error) {
+				return acc;
+			}
+
+			const formattedErrorMessage = error.replace(/"/g, "");
+
+			return [...acc, formattedErrorMessage];
+		}, [] as string[]);
+
+		return res.status(400).json({
+			message: errors[0], // return only the first error
+		});
+	}
+
+	return res.status(500).json({
+		message: "Internal server error: " + err?.message ?? "not specified",
+	});
+};
+
+export { errorHandler };
