10549616b07af9dafd476a506f447acd326ee2b3
ryansldev <ryanlima1309@gmail.com>
2022-07-09 13:40:39 -0300

    feat: add task routes

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,9 +1,26 @@
+import { celebrate, Joi } from 'celebrate'
 import { Router } from 'express'
+import { TaskController } from '~controllers/Task.controller'
 
+const taskController = new TaskController()
 const router = Router()
 
-router.get('/', (req, res) => {
-  res.status(200).json({ ok: true })
-})
+router.post('/tasks', celebrate({
+  body: {
+    title: Joi.string().not().empty().required(),
+    description: Joi.string().not().empty().required(),
+    done: Joi.boolean().not().empty().required()
+  }
+}), taskController.create)
+router.get('/tasks', taskController.list)
+router.get('/tasks/:id', taskController.find)
+router.put('/tasks/:id', celebrate({
+  body: {
+    title: Joi.string(),
+    description: Joi.string(),
+    done: Joi.boolean()
+  }
+}), taskController.update)
+router.delete('/tasks/:id', taskController.delete)
 
 export { router }
