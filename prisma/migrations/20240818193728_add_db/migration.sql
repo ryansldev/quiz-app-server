1160242dca438fcce085ca7541c05a9cacfe07c9
ryansldev <ryanlima1309@gmail.com>
2024-08-18 16:37:38 -0300

    fix: deploy

---
+++ prisma/migrations/20240818193728_add_db/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "Task" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT,
+    "done" BOOLEAN NOT NULL,
+    "deadline" TIMESTAMP(3),
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Task_pkey" PRIMARY KEY ("id")
+);
