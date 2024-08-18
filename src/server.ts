d381d3a7bbd22289bc33662e36f52c8a5ee3bc48
ryansldev <ryanlima1309@gmail.com>
2024-08-18 17:01:51 -0300

    fix: trying to fix deploy

--- src/server.ts
+++ src/server.ts
@@ -17,6 +17,18 @@ const app = express()
 app.use(cors())
 
 app.use(express.json())
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header(
+    'Access-Control-Allow-Methods',
+    'GET,HEAD,OPTIONS,POST,PUT,DELETE'
+  )
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  )
+  next()
+})
 
 const server = createServer(app)
 const io = new Server(server, {
